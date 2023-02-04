5bd6a8bd188db5e5eb0f4cf81dbedf77c785a30c
code-l <luizy991212@gmail.com>
2023-02-04 17:14:26 +0900

    programmers 118670 edit

--- src/main/java/org/example/Programmers118670.java
+++ src/main/java/org/example/Programmers118670.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-
+import java.util.*;
 public class Programmers118670 {
 
     public static void main(String[] args) {
@@ -16,16 +14,34 @@ public class Programmers118670 {
             rc = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
             operations = new String[]{"Rotate", "ShiftRow"};
             int[][] answer = rc;
-
             ArrayDeque<ArrayDeque<Integer>> ans = new ArrayDeque<>();
 
-            for (int i = 0; i < answer.length; i++) {
-                ans.addLast(new ArrayDeque(Arrays.asList(answer[i])));
+            for (int i = 0; i < rc.length; i++) {
+                ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+                for (int j = 0; j < rc[i].length; j++) {
+                    arrayDeque.addLast(rc[i][j]);
+                }
+                ans.addLast(arrayDeque);
             }
 
             for (String str : operations) {
                 if (str.equals("Rotate")){
-
+                    Iterator<ArrayDeque<Integer>> it = ans.iterator();
+                    ArrayDeque<Integer> pre = it.next();
+                    ArrayDeque<Integer> cnt = null;
+                    Integer preLast = pre.pollLast();
+                    Integer cntLast = null;
+                    for (int i = 1 ; i < ans.size() - 1; i++) {
+                        cnt = it.next();
+                        cntLast = cnt.pollLast();
+                        cnt.addLast(preLast);
+                        pre.addFirst(cnt.pollFirst());
+                        pre = cnt;
+                        preLast = cntLast;
+                    }
+                    cnt = it.next();
+                    cnt.addLast(preLast);
+                    pre.addFirst(cnt.pollFirst());
                 } else if (str.equals("ShiftRow")) {
                     ans.addFirst(ans.pollLast());
                 }
@@ -36,11 +52,9 @@ public class Programmers118670 {
                 answer[i] = tmp.stream().mapToInt(Integer::intValue).toArray();
             }
 
-
             for (int[] arr : rc) {
                 System.out.println(Arrays.toString(arr));
             }
-            
 
             return answer;
         }
