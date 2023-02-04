9d5afe8d5b88ca49fe70ca8415cec13bdbdaee11
code-l <luizy991212@gmail.com>
2023-02-04 18:22:24 +0900

    programmers 118670 complete

--- src/main/java/org/example/Programmers118670.java
+++ src/main/java/org/example/Programmers118670.java
@@ -11,14 +11,19 @@ public class Programmers118670 {
 
     static class Solution {
         public int[][] solution(int[][] rc, String[] operations) {
-            rc = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-            operations = new String[]{"Rotate", "ShiftRow"};
-            int[][] answer = rc;
+            rc = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+            operations = new String[]{"ShiftRow", "Rotate", "ShiftRow", "Rotate"};
+            final int ROW = rc[0].length;
             ArrayDeque<ArrayDeque<Integer>> ans = new ArrayDeque<>();
+
+            ArrayDeque<Integer> ansFirst = new ArrayDeque<>();
+            ArrayDeque<Integer> ansLast = new ArrayDeque<>();
 
             for (int i = 0; i < rc.length; i++) {
                 ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
-                for (int j = 0; j < rc[i].length; j++) {
+                ansFirst.addLast(rc[i][0]);
+                ansLast.addLast(rc[i][ROW-1]);
+                for (int j = 1; j < ROW - 1; j++) {
                     arrayDeque.addLast(rc[i][j]);
                 }
                 ans.addLast(arrayDeque);
@@ -26,37 +31,49 @@ public class Programmers118670 {
 
             for (String str : operations) {
                 if (str.equals("Rotate")){
-                    Iterator<ArrayDeque<Integer>> it = ans.iterator();
-                    ArrayDeque<Integer> pre = it.next();
-                    ArrayDeque<Integer> cnt = null;
-                    Integer preLast = pre.pollLast();
-                    Integer cntLast = null;
-                    for (int i = 1 ; i < ans.size() - 1; i++) {
-                        cnt = it.next();
-                        cntLast = cnt.pollLast();
-                        cnt.addLast(preLast);
-                        pre.addFirst(cnt.pollFirst());
-                        pre = cnt;
-                        preLast = cntLast;
-                    }
-                    cnt = it.next();
-                    cnt.addLast(preLast);
-                    pre.addFirst(cnt.pollFirst());
+                    ans.getFirst().addFirst(ansFirst.pollFirst());
+                    ansLast.addFirst(ans.getFirst().pollLast());
+                    ans.getLast().addLast(ansLast.pollLast());
+                    ansFirst.addLast(ans.getLast().pollFirst());
+
+
+//                    Iterator<ArrayDeque<Integer>> it = ans.iterator();
+//                    ArrayDeque<Integer> pre = it.next();
+//                    ArrayDeque<Integer> cnt = null;
+//                    Integer preLast = pre.pollLast();
+//                    Integer cntLast = null;
+//                    for (int i = 1 ; i < ans.size() - 1; i++) {
+//                        cnt = it.next();
+//                        cntLast = cnt.pollLast();
+//                        cnt.addLast(preLast);
+//                        pre.addFirst(cnt.pollFirst());
+//                        pre = cnt;
+//                        preLast = cntLast;
+//                    }
+//                    cnt = it.next();
+//                    cnt.addLast(preLast);
+//                    pre.addFirst(cnt.pollFirst());
                 } else if (str.equals("ShiftRow")) {
                     ans.addFirst(ans.pollLast());
+                    ansFirst.addFirst(ansFirst.pollLast());
+                    ansLast.addFirst(ansLast.pollLast());
                 }
             }
 
-            for (int i = 0; i < answer.length; i++) {
-                ArrayDeque<Integer> tmp = ans.pollFirst();
-                answer[i] = tmp.stream().mapToInt(Integer::intValue).toArray();
+            for (int i = 0; i < rc.length; i++) {
+                rc[i][0] = ansFirst.pollFirst();
+                ArrayDeque<Integer> arrayDeque = ans.pollFirst();
+                for (int j = 1; j < ROW - 1; j++) {
+                    rc[i][j] = arrayDeque.pollFirst();
+                }
+                rc[i][ROW-1] = ansLast.pollFirst();
             }
 
             for (int[] arr : rc) {
                 System.out.println(Arrays.toString(arr));
             }
 
-            return answer;
+            return rc;
         }
     }
 }
