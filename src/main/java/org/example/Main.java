a8c38388d765d9884df416a7f05c25ab299e9d82
code-l <luizy991212@gmail.com>
2023-02-06 22:12:47 +0900

    Beak7569 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,42 +3,42 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int c = Integer.parseInt(br.readLine());
-        for (int i = 0; i < c; i++) {
-            int k = Integer.parseInt(br.readLine());
-            boolean[][] point = new boolean[k+1][];
-            for (int j = 1; j <= k; j++) {
-                point[j] = new boolean[j];
-            }
-            for (int j = 1; j < k; j++) {
-                for (int l = j + 1; l <= k; l++) {
-                    if (point[l][j])
-                        continue;
-                    for (int m = j*2, n = l*2; n <= k; m+=j, n+=l) {
-                        point[n][m] = true;
-                    }
-                }
-            }
-            int ans = 0;
-            for (int j = 1; j < k; j++) {
-                for (int l = j+1; l <= k; l++) {
-                    if (!point[l][j])
-                        ans++;
-                }
-            }
-            System.out.println(ans*2 +3);
-            // out
-            for (int j = 1; j < point.length; j++) {
-                for (int l = 1; l < point[j].length; l++) {
-                    System.out.printf(point[j][l]+",");
-                }
-                System.out.println();
-            }
+        int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        int cnt = 0;
+        switch (n % 10){
+            case 5: case 0:
+                cnt = n/5;
+                break;
+            case 2 : case 7:
+                if(n<12)
+                    cnt = -1;
+                else
+                    cnt = (n-12)/5 + 4;
+                break;
+            case 3: case 8:
+                if (n<3)
+                    cnt = -1;
+                else
+                    cnt = (n-3)/5 + 1;
+                break;
+            case 6: case 1:
+                if (n<6)
+                    cnt = -1;
+                else
+                    cnt = (n-6)/5 + 2;
+                break;
+            case 9: case 4:
+                if (n<9)
+                    cnt = -1;
+                else
+                    cnt = (n-9)/5 + 3;
+                break;
         }
+        System.out.println(cnt);
+
     }
 }
