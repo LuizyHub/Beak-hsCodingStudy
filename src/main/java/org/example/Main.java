9c46ee280ad64e0722b34a0ee4a85e2980d2a83a
code-l <luizy991212@gmail.com>
2023-03-03 15:49:00 +0900

    Beak2143 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,34 @@
 package org.example;
 
 public class Main {
+    private static int binarySearch(int[] a, int key){
+        return binarySearch0(a,0,a.length,key);
+    }
+    private static int binarySearch0(int[] a, int fromIndex, int toIndex,
+                                     int key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            if (low == high && a[low] == key)
+                return low;
+            int mid = (low + high) >>> 1;
+            int midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else //midVal == key
+                high = mid;
+        }
+        return -(low+1);  // key not found.
+    }
     public static void main(String[] args) {
-        long num = 9;
-        for (int i = 1;num < 1_000_000_000_000_000_000l ; i++) {
-            System.out.println(i + " , " + num );
-            num *= 9;
+        int[] arr = {0,0,0,0,2,2,2,4,4,5,5,6,8,9,9};
+        for (int i = -1; i < 10; i++) {
+            System.out.println(i + ":" + binarySearch(arr,i));
+
         }
     }
 }
