e3a6b7375299c237bce01e5d61b4aaa665a63a7a
luizy <luizy991212@gmail.com>
2023-03-04 18:24:18 +0900

    Beak17143 done

--- src/main/java/Beak17143.java
+++ src/main/java/Beak17143.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Beak17143 {
@@ -107,7 +105,6 @@ public class Beak17143 {
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         R = read();
         C = read();
         matrix = new int[R][C];
