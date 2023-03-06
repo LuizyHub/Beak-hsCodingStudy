4c0827a4953800330ae2d70005972cd569aba332
luizy <luizy991212@gmail.com>
2023-03-06 20:07:28 +0900

    Beak17387 done

--- src/main/java/Beak17387.java
+++ src/main/java/Beak17387.java
@@ -4,11 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Beak17387 {
-//    private static int gcd(int p, int q)
-//    {
-//        if (q == 0) return p;
-//        return gcd(q, p%q);
-//    }
+    private static Dot[] dots;
+    private static int ans = 0;
     private static class Dot{
         public long x,y;
 
@@ -16,65 +13,56 @@ public class Beak17387 {
             this.x = x;
             this.y = y;
         }
-
-//        @Override
-//        public String toString() {
-//            final StringBuilder sb = new StringBuilder("Dot{");
-//            sb.append("x=").append(x);
-//            sb.append(", y=").append(y);
-//            sb.append('}');
-//            return sb.toString();
-//        }
     }
-//    private static boolean isParallel
+    private static int sign(long num){
+        if (num > 0)
+            return 1;
+        else if (num == 0)
+            return 0;
+        else
+            return -1;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        Dot[] dots = new Dot[4];
+        dots = new Dot[4];
         st = new StringTokenizer(br.readLine());
         dots[0] = new Dot(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
-        dots[1] = new Dot(Integer.parseInt(st.nextToken()) - dots[0].x,Integer.parseInt(st.nextToken()) - dots[0].y);
-        int xsign = 1,ysign = 1;
-        if (dots[1].x < 0){
-            xsign = -1;
-            dots[1].x *= xsign;
-        }
-        if (dots[1].y < 0){
-            ysign = -1;
-            dots[1].y *= ysign;
-        }
+        dots[1] = new Dot(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
         st = new StringTokenizer(br.readLine());
-        dots[2] = new Dot(xsign*(Integer.parseInt(st.nextToken()) - dots[0].x),ysign*(Integer.parseInt(st.nextToken()) - dots[0].y));
-        dots[3] = new Dot(xsign*(Integer.parseInt(st.nextToken()) - dots[0].x),ysign*(Integer.parseInt(st.nextToken()) - dots[0].y));
-        dots[0] = new Dot(0,0);
-        //        System.out.println((x1*x2 + y1*y2) > 0 ? 0 : 1);
-//        System.out.println(Arrays.toString(dots));
-        int ans = 0;
-        long rel1 = dots[2].y*dots[1].x - dots[1].y*dots[2].x;
-        long rel0 = dots[3].y*dots[1].x - dots[1].y*dots[3].x;
-        if (!((rel0<0 && rel1<0) || (rel0>0 && rel1>0))){
-            rel1 = (dots[1].x-dots[2].x)*(dots[3].y-dots[2].y) - (dots[1].y - dots[2].y)*(dots[3].x - dots[2].x); // 4000000*4000000*2 = 32,000,000,000,000
-            rel0 = (dots[0].x-dots[2].x)*(dots[3].y-dots[2].y) - (dots[0].y - dots[2].y)*(dots[3].x - dots[2].x);
-            if (!((rel0<0 && rel1<0) || (rel0>0 && rel1>0)))
-                ans = 1;
+        dots[2] = new Dot(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+        dots[3] = new Dot(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+        int rel012 = sign((dots[2].x-dots[0].x)*(dots[1].y-dots[0].y) - (dots[2].y - dots[0].y)*(dots[1].x - dots[0].x)); //012
+        int rel013 = sign((dots[3].x-dots[0].x)*(dots[1].y-dots[0].y) - (dots[3].y - dots[0].y)*(dots[1].x - dots[0].x)); //013
+        int rel123 = sign((dots[1].x-dots[2].x)*(dots[3].y-dots[2].y) - (dots[1].y - dots[2].y)*(dots[3].x - dots[2].x)); //123
+        int rel023 = sign((dots[0].x-dots[2].x)*(dots[3].y-dots[2].y) - (dots[0].y - dots[2].y)*(dots[3].x - dots[2].x)); //023
+        if (rel012 * rel013 <= 0 && rel023 * rel123 <= 0){
+            if (rel012 == 0 && rel123 == 0){
+                if (((dots[0].x - dots[2].x)*(dots[1].x - dots[2].x) <= 0) && ((dots[0].y - dots[2].y)*(dots[1].y - dots[2].y) <= 0)
+                || ((dots[0].x - dots[3].x)*(dots[1].x - dots[3].x) <= 0) && ((dots[0].y - dots[3].y)*(dots[1].y - dots[3].y) <= 0)
+                || ((dots[2].x - dots[1].x)*(dots[3].x - dots[1].x) <= 0) && ((dots[2].y - dots[1].y)*(dots[3].y - dots[1].y) <= 0)
+                || ((dots[2].x - dots[0].x)*(dots[3].x - dots[0].x) <= 0) && ((dots[2].y - dots[0].y)*(dots[3].y - dots[0].y) <= 0))
+                    ans = 1;
+            }
+            else
+                ans =1;
         }
-//        if ((dots[2].y*dots[1].x - dots[1].y*dots[2].x)*(dots[3].y*dots[1].x - dots[1].y*dots[3].x) <= 0){
-//            rel1 = (dots[1].x-dots[2].x)*(dots[3].y-dots[2].y) - (dots[1].y - dots[2].y)*(dots[3].x - dots[2].x); // 4000000*4000000*2 = 32,000,000,000,000
-//            rel0 = (dots[0].x-dots[2].x)*(dots[3].y-dots[2].y) - (dots[0].y - dots[2].y)*(dots[3].x - dots[2].x);
-//            if (rel0*rel1 <= 0)
+
+//        if (rel012 == 0 && ((dots[0].x - dots[2].x)*(dots[1].x - dots[2].x) <= 0) && ((dots[0].y - dots[2].y)*(dots[1].y - dots[2].y) <= 0)){ // if 012 parallel and 2 is between 01
 //                ans = 1;
-//
-//////            long rel2 = dots[2].y*dots[1].y + dots[1].x*dots[2].x;
-////            long rel2 = dots[1].y*dots[2].y + dots[2].x*dots[1].x;
-//////            System.out.println(rel2);
-////            if (0 <= rel2){
-//////                long rel3 = dots[3].y*dots[1].y + dots[1].x*dots[3].x;
-////                long rel3 = dots[1].y*dots[3].y + dots[3].x*dots[1].x;
-//////                System.out.println(rel3);
-////                if (0 <= rel3)
-////                    ans = 1;
-////            }
-//
+//        }
+//        else if (rel013 == 0 && ((dots[0].x - dots[3].x)*(dots[1].x - dots[3].x) <= 0) && ((dots[0].y - dots[3].y)*(dots[1].y - dots[3].y) <= 0)){ // if 013 parallel and 3 is between 01
+//                ans = 1;
+//        }
+//        else if (((rel012<0 && rel013>0) || (rel012>0 && rel013<0))){ // 2,3 is other side of line 01
+//            if (rel123 == 0 && ((dots[2].x - dots[1].x)*(dots[3].x - dots[1].x) <= 0) && ((dots[2].y - dots[1].y)*(dots[3].y - dots[1].y) <= 0)){ // if 123 parallel and 1 is between 23
+//                    ans = 1;
+//            }
+//            else if (rel023 == 0 && ((dots[2].x - dots[0].x)*(dots[3].x - dots[0].x) <= 0) && ((dots[2].y - dots[0].y)*(dots[3].y - dots[0].y) <= 0)) { // if 023 parallel and 0 is between 23
+//                    ans = 1;
+//            }
+//            else if (((rel123<0 && rel023>0) || (rel123>0 && rel023<0))) // 0,1 is other side of line 23
+//                ans = 1;
 //        }
         System.out.println(ans);
     }
