225ebfc255d821fe832e32dfa27eb21bafd14a6a
luizy <luizy991212@gmail.com>
2023-03-08 19:38:01 +0900

    in school

---
+++ src/main/java/Beak2239.java
@@ -0,0 +1,80 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Beak2239 {
+    private static int[][] availList = new int[9][9];
+    private static int[][] sdoku = new int[9][9];
+    private static class Node implements Comparable<Node>{
+        public int idx;
+        public int bitCount;
+
+        public Node(int idx, int bitCount) {
+            this.idx = idx;
+            this.bitCount = bitCount;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            int dif = Integer.compare(o.bitCount, this.bitCount);
+            if (dif == 0)
+                return Integer.compare(this.idx, o.idx);
+            return dif;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sdoku[i][j] = System.in.read() - 48;
+            }
+            System.in.read();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                int num = sdoku[i][j];
+                if (num == 0) continue;
+                for (int k = 0; k < 9; k++) {
+                    availList[k][j] |= (1<<num);
+                    availList[i][k] |= (1<<num);
+                }
+                for (int k = (i/3)*3; k < 3; k++) {
+                    for (int l = (j/3)*3; l < 3; l++) {
+                        availList[k][l] |= (1<<num);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (sdoku[i][j] == 0){
+                    System.out.print("[" + i + "," + j + "]");
+                    for (int k = 1; k <= 9; k++) {
+                        if (((availList[i][j]>>k) & 1) == 0)
+                            System.out.print(k);
+                    }
+                    System.out.println();
+                }
+            }
+        }
+
+
+        for (int[] ints : sdoku) {
+            System.out.println(Arrays.toString(ints));
+        }
+
+    }
+}
+/*
+000500163
+049013507
+500620804
+490105382
+218000400
+050208900
+000000008
+005706200
+300900600
+
+ */
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,64 +1,14 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
+public class Main{
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(511));
+        int a = 0b0;
+        a = a | 1<<9;
+        a = a | 1<<7;
 
-public class Main {
-    static class Node {
-        int n;
-        int cnt;
-
-        public StringBuilder sb;
-
-        public Node(int n, int cnt, StringBuilder sb) {
-            this.n = n;
-            this.cnt = cnt;
-            this.sb = sb;
-        }
-    }
-
-
-    public static int n;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-
-        boolean[] check = new boolean[n + 1];
-        ArrayDeque<Node> dq = new ArrayDeque<>();
-        dq.add(new Node(n, 0, new StringBuilder(Integer.toString(n))));
-
-        while(!dq.isEmpty()) {
-            Node node = dq.pollFirst();
-
-            if (node.n == 1) {
-                System.out.print(node.cnt + "\n" + node.sb);
-                break;
-            }
-
-
-
-            int[] calc;
-            if (node.n % 2 == 0 && node.n % 3 == 0) {
-                calc = new int[]{node.n / 3, node.n / 2, node.n - 1};
-            } else if (node.n % 2 == 0) {
-                calc = new int[]{node.n / 2, node.n - 1};
-            } else if (node.n % 3 == 0) {
-                calc = new int[]{node.n / 3, node.n - 1};
-            } else {
-                calc = new int[]{node.n - 1};
-            }
-
-            for (int next: calc) {
-                if (next < 1) continue;
-                if (check[next]) continue;
-                check[next] = true;
-
-//                dq.addLast(new Node(next, node.cnt + 1));
-            }
-        }
-
+        a = a & 1<<3;
+        System.out.println(Integer.bitCount(a));
+        System.out.println(Integer.toBinaryString(a));
     }
 }
