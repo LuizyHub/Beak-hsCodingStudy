d38cc68063c921881a9eeb24e68d7bb5c57e0e53
luizy <luizy991212@gmail.com>
2023-03-12 12:57:31 +0900

    Beak15863 done

--- src/main/java/Beak2239.java
+++ src/main/java/Beak2239.java
@@ -1,29 +1,86 @@
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Beak2239 {
     private static int[][] availList = new int[9][9];
     private static int[][] sdoku = new int[9][9];
-    private static class Node implements Comparable<Node>{
-        public int idx;
-        public int bitCount;
-
-        public Node(int idx, int bitCount) {
-            this.idx = idx;
-            this.bitCount = bitCount;
+    private static int searchNum(int bitmask, int start){
+        for (int i = start; i <= 9; i++) {
+            if (((bitmask >> i) & 1) == 0){
+                return i;
+            }
         }
+        return 0; // if not found;
+    }
 
-        @Override
-        public int compareTo(Node o) {
-            int dif = Integer.compare(o.bitCount, this.bitCount);
-            if (dif == 0)
-                return Integer.compare(this.idx, o.idx);
-            return dif;
+    private static int searchNum(int bitmask){
+        return searchNum(bitmask,1);
+    }
+
+    private static void renewList(int[][] checkList, int x, int y, int num){
+        for (int j = 0; j < 9; j++) {
+            checkList[x][j] |= (1<<num);
+            checkList[j][y] |= (1<<num);
+        }
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                checkList[(x / 3) * 3 + j][(y / 3) * 3 + k] |= (1<<num);
+            }
         }
     }
-    private static void fill(){
-
+    private static void fill(int[][] list, int[][] board){
+        int maxbit = 8;
+        int x = 0, y = 0;
+        boolean ischanged = false;
+        while (maxbit == 8) {
+            ischanged = false;
+            maxbit = -1;
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (board[i][j] == 0){
+                        int bitCount = Integer.bitCount(list[i][j]);
+                        if (bitCount > maxbit){
+                            maxbit = bitCount;
+                        }
+                        if (bitCount == 8){
+                            int num = searchNum(list[i][j]);
+                            renewList(list,i,j,num);
+                            board[i][j] = num;
+                        }else if (!ischanged){
+                            x = i; y = j;
+                        }
+                        ischanged = true;
+                    }
+                }
+            }
+        }
+        if (!ischanged){
+            StringBuilder sb = new StringBuilder();
+            for (int[] ints : board) {
+                for (int i : ints) {
+                    sb.append(i);
+                }
+                sb.append('\n');
+            }
+            System.out.print(sb);
+            System.exit(0);
+            //completed
+        } else {
+            int num = searchNum(list[x][y]);
+            while (num != 0){
+                int[][] nextBoard = new int[9][];
+                int[][] nextList = new int[9][];
+                for (int i = 0; i < 9; i++) {
+                    nextBoard[i] = board[i].clone();
+                    nextList[i] = list[i].clone();
+                }
+                renewList(nextList,x,y,num);
+                nextBoard[x][y] = num;
+                fill(nextList,nextBoard);
+                num = searchNum(list[x][y], num + 1);
+            }
+        }
     }
+
 
     public static void main(String[] args) throws IOException {
         for (int i = 0; i < 9; i++) {
@@ -41,34 +98,31 @@ public class Beak2239 {
                     availList[k][j] |= (1<<num);
                     availList[i][k] |= (1<<num);
                 }
-                for (int k = (i/3)*3; k < 3; k++) {
-                    for (int l = (j/3)*3; l < 3; l++) {
-                        availList[k][l] |= (1<<num);
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        availList[(i/3)*3 + k][(j/3)*3 + l] |= (1<<num);
                     }
                 }
             }
         }
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (sdoku[i][j] == 0){
-                    System.out.print("[" + i + "," + j + "]");
-                    for (int k = 1; k <= 9; k++) {
-                        if (((availList[i][j]>>k) & 1) == 0)
-                            System.out.print(k);
-                    }
-                    System.out.println();
-                }
-            }
-        }
-
-
-        for (int[] ints : sdoku) {
-            System.out.println(Arrays.toString(ints));
-        }
+        fill(availList,sdoku);
 
     }
 }
+/*
+000500163
+049013507
+500620804
+490105382
+218000400
+050208900
+000000008
+005706200
+300900600
+
+ */
+
 /*
 000500163
 049013507
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,7 @@
 package org.example;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(511));
-        int a = 0b0;
-        a = a | 1<<9;
-        a = a | 1<<7;
-
-        a = a & 1<<3;
-        System.out.println(Integer.bitCount(a));
-        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString( ((1<<(5-1)) - 1) << 10 ));
     }
 }
