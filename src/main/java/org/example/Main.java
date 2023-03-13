0c2d8cfd6df7512ba81fbed2dddb7592b9957b22
luizy <luizy991212@gmail.com>
2023-03-13 17:34:35 +0900

    Beak23291 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString( ((1<<(5-1)) - 1) << 10 ));
+        int i = 2143;
+        System.out.println((int) Math.ceil(Math.log(i) / Math.log(2)));
+        System.out.println(Integer.toBinaryString(i).length());
+
+        System.out.println(Integer.toBinaryString(-i));
+        System.out.println(Integer.toBinaryString(i));
+
+        System.out.println(i & ~(i & (-i)));
+
+        System.out.println((int)Math.pow(2,10));
+        System.out.println(1<<10);
+
+
     }
 }
