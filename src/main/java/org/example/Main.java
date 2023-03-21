ab09f61cbf2425adcfc50a74e082ee873366dcea
luizy <luizy991212@gmail.com>
2023-03-22 02:02:30 +0900

    save

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,9 @@
 package org.example;
 
 public class Main {
+    static final int MAX = 1_000_000;
     public static void main(String[] args) {
-        System.out.println((int)'-');
-        int i = 2143;
-        System.out.println((int) Math.ceil(Math.log(i) / Math.log(2)));
-        System.out.println(Integer.toBinaryString(i).length());
-
-        System.out.println(Integer.toBinaryString(-i));
-        System.out.println(Integer.toBinaryString(i));
-
-        System.out.println(i & ~(i & (-i)));
-
-        System.out.println((int)Math.pow(2,10));
-        System.out.println(1<<10);
-
-
+        System.err.println("err");
+        System.out.println("out");
     }
 }
