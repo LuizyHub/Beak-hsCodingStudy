d80d2f6f5a9c2384f97ae5b2d1e9493c62c66f33
luizy <luizy991212@gmail.com>
2023-03-22 23:39:23 +0900

    Beak13460 run

--- src/main/java/Beak1208.java
+++ src/main/java/Beak1208.java
@@ -1,21 +1,81 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Beak1208 {
-    private static int N, S, count = 0;
-    private static int[] arr;
+    private static int N, S;
+    private static long count = 0;
 
-    private static void solution(int idx, int target){
-        for (int i = idx - 1; i >= 0; i--) {
-            int periodSum = arr[idx] - arr[i];
-            System.out.println(target + " " + idx + " " + i);
-            System.out.println(periodSum - target + S);
-            if (periodSum == target)
+
+    private static ArrayList<Long> getAvailList(ArrayList<Long> list, int start, long beforesum, long size){
+        if (size == 1){
+            return new ArrayList<Long>();
+        }
+        ArrayList<Long> returnList = new ArrayList<>();
+        for (int i = start; i < list.size(); i++) {
+            long num = list.get(i) + beforesum;
+            if (num < 0)
+                returnList.add(num*100 - size + 1);
+            else
+                returnList.add(num*100 + size - 1);
+            returnList.addAll(getAvailList(list, i+1, num,size -1));
+        }
+        return returnList;
+    }
+    private static long com(long a, long b){
+        b = Math.min(b, a - b);
+        if (a >= 30)
+            if (b > 0)
+                return com(a-1, b) + com(a-1, b-1);
+            else
+                return com(a-1, b);
+        long ans = 1;
+        for (int i = 0; i < b; i++) {
+            ans *= (a - i);
+        }
+        for (int i = 1; i <= b; i++) {
+            ans /= i;
+        }
+        return ans;
+    }
+    private static ArrayList<Long> arr;
+
+    private static void solution(long size, long target, ArrayList<Long> list){
+
+
+        if (list.get(0) == list.get(list.size() - 1) && list.get(0)*size == target){
+            count += com(list.size(),size);
+            return;
+        }
+        if (size == 1){
+            if (Collections.binarySearch(list, (long)target) >= 0)
                 count++;
-            solution(i - 1, target - periodSum);
+            return;
+        }
+
+        double mean = 1.0 *target / size;
+        ArrayList<Long> small = new ArrayList<>();
+        ArrayList<Long> big = new ArrayList<>();
+        for (Long l : list) {
+            if (l > mean)
+                big.add(l);
+            else
+                small.add(l);
+        }
+        if (big.size() == 0 || small.size() == 0)
+            return;
+        if (big.size() < small.size()){
+            for (Long l : getAvailList(big, 0, 0, size)) {
+                if (Math.abs(l%100) <= small.size())
+                    solution(Math.abs(l%100), target - l/100, small);
+            }
+        }
+        else {
+            for (Long l : getAvailList(small, 0, 0, size)) {
+                if (Math.abs(l%100) <= big.size())
+                    solution(Math.abs(l%100), target - l/100, big);
+            }
         }
     }
 
@@ -24,18 +84,28 @@ public class Beak1208 {
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        arr = new int[N];
+        arr = new ArrayList<>();
         S = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr.add(Long.parseLong(st.nextToken()));
         }
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-        solution(N,S);
+        Collections.sort(arr);
+//        System.out.println(arr);
+//        System.out.println(getAvailList(arr, 0, 0, 5));
+//        for (Long aLong : getAvailList(arr,0,0,5)) {
+//            System.out.println(aLong%100 + " " + aLong/100);
+//        }
+//        System.out.println(-696%100);
+//        System.out.println((-696)/100);
 
-//        System.out.print(count);
+        for (int i = 1; i <= N; i++) {
+            solution(i, S, arr);
+        }
+
+
+        System.out.print(count);
     }
 }
 
--- src/main/java/Beak15684.java
+++ src/main/java/Beak15684.java
@@ -22,22 +22,23 @@ public class Beak15684 {
         return true;
     }
 
-//    private static void printStair(){
-//        for (int i = 1; i <= N; i++) {
-//            System.out.print(" " + i);
-//        }
-//        System.out.println();
-//        for (int i = 0; i < M; i++) {
-//            System.out.print((i+1) + " ");
-//            for (int j = 1; j < N ; j++) {
-//                if (stairs[i][j])
-//                    System.out.print("_ ");
-//                else
-//                    System.out.print(". ");
-//            }
-//            System.out.println();
-//        }
-//    }
+    private static void printStair(){
+        System.out.println();
+        for (int i = 1; i <= N; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        for (int i = 0; i < M; i++) {
+            System.out.print((i+1) + " ");
+            for (int j = 1; j < N ; j++) {
+                if (stairs[i][j])
+                    System.out.print("_ ");
+                else
+                    System.out.print(". ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -65,6 +66,7 @@ public class Beak15684 {
             int yi = i % mod + 1;
             if (stairs[xi][yi-1] || stairs[xi][yi] || stairs[xi][yi+1]) continue;
             stairs[xi][yi] = true;
+            printStair();
             if (solution()){
                 stairs[xi][yi] = false;
                 answer = 1;
@@ -75,6 +77,7 @@ public class Beak15684 {
                 int yj = (j % mod) + 1;
                 if (stairs[xj][yj-1] || stairs[xj][yj] || stairs[xj][yj+1]) continue;
                 stairs[xj][yj] = true;
+                printStair();
                 if (solution()){
                     stairs[xj][yj] = false;
                     answer = 2;
@@ -85,6 +88,7 @@ public class Beak15684 {
                     int yk = (k % mod) + 1;
                     if (stairs[xk][yk - 1] || stairs[xk][yk] || stairs[xk][yk + 1]) continue;
                     stairs[xk][yk] = true;
+                    printStair();
                     if (solution()) {
                         stairs[xk][yk] = false;
                         answer = 3;
